2df3c76d90c2b413aa1fa7e4e37d281356e8ff1b
Ray Winkelman <raywinkelman@gmail.com>
2016-05-01 00:31:57 -0300

    Now persisting to my Google datastore project.

--- API/app/Models/Farm.php
+++ API/app/Models/Farm.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use GDS\Entity;
+use GDS\Store;
 
-class Farm extends GDS\Entity
+class Farm extends Entity
 {
 
     /**
@@ -20,17 +21,49 @@ class Farm extends GDS\Entity
       //     'isbn' => '1840224313'
       // ]);
 
-        $this->setEntityClass('\\Book');
+        //$this->setEntityClass('\\Book');
+        //
+        // $obj_book = new Entity();
+        // $obj_book->title = 'Romeo and Juliet';
+        // $obj_book->author = 'William Shakespeare';
+        // $obj_book->isbn = '1840224339';
+        // //
+        // // // Write it to Datastore
+        // // $obj_store = new Store('Book');
+        // // $obj_store->upsert($obj_book);
+        //
+        // // return (new GDS\Schema('Book'))
+        // //     ->addString('title')
+        // //     ->addString('author')
+        // //     ->addString('isbn', TRUE);
+        //
+        // $obj_client = \GDS\Gateway\GoogleAPIClient::createClientFromJson('/gdskey.json');
+        // $obj_gateway = new \GDS\Gateway\GoogleAPIClient($obj_client, 'laravel-api');
+        // $obj_store = new \GDS\Store('Book', $obj_gateway);
+        //
+        // $obj_store->upsert($obj_book);
+//GDS_APP_NAME, GDS_SERVICE_ACCOUNT_NAME, GDS_KEY_FILE_PATH
+        $obj_client = \GDS\Gateway\GoogleAPIClient::createClientFromJson('../gdskey.json');
 
-        return $this->createEntity([
-            'title' => 'Some Book',
-            'author' => 'A N Other Guy',
-            'isbn' => '1840224313'
-        ]);
+        // Gateway requires a Google_Client and Dataset ID
+        $obj_gateway = new \GDS\Gateway\GoogleAPIClient($obj_client, 'laravel-api');
 
-        // return (new GDS\Schema('Book'))
-        //     ->addString('title')
-        //     ->addString('author')
-        //     ->addString('isbn', TRUE);
+        // Define our Model Schema
+        $obj_book_schema = (new \GDS\Schema('Book'))
+            ->addString('title')
+            ->addString('author')
+            ->addString('isbn', TRUE)
+            ->addDatetime('published', FALSE)
+            ->addString('text', FALSE);
+
+        // Store requires a Gateway and Schema
+        $obj_book_store = new \GDS\Store($obj_book_schema, $obj_gateway);
+
+        $obj_book = new Entity();
+        $obj_book->title = 'Romeo and Juliet';
+        $obj_book->author = 'William Shakespeare';
+        $obj_book->isbn = '1840224339';
+
+        $obj_book_store->upsert($obj_book);
     }
 }
